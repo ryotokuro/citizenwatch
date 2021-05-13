3b0e56b8a394312c13f233e62e889068ae450650
ryotokuro <ryotokuro@gmail.com>
2021-05-13 11:43:36 +1000

    app now starts on login screen, fixing auth, changing to new identity pool in next update

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
+import { StatusBar } from 'expo-status-bar';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+//import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { withAuthenticator } from 'aws-amplify-react-native';
 import useCachedResources from './hooks/useCachedResources';
 import useColorScheme from './hooks/useColorScheme';
 import Navigation from './navigation';
+
+// Amplify modules
 import Amplify from 'aws-amplify';
 import config from './aws-exports';
-import Home from './screens/Home';
-import Capture from './screens/Capture';
+
+// React library modules
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+// User defined files
+import Home from './screens/Home';
+import Capture from './screens/Capture';
 
 Amplify.configure({
-  ...config,
-  Analytics: {
-    disabled: true,
-  },
+	...config,
+	Analytics: {
+		disabled: true,
+	},
 });
 
 function App() {
-  const Stack = createStackNavigator();
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
+	const Stack = createStackNavigator();
+	const isLoadingComplete = useCachedResources();
+	const colorScheme = useColorScheme();
 
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar style="auto" />
-      </SafeAreaProvider>
-    );
-  }
+	if (!isLoadingComplete) {
+		return null;
+	} else {
+		return (
+			<SafeAreaProvider>
+			<Navigation colorScheme={colorScheme} />
+			<StatusBar style="auto" />
+			</SafeAreaProvider>
+		);
+	}
 }
-//export default withAuthenticator(App);
-export default App;
+
+export default withAuthenticator(App);
